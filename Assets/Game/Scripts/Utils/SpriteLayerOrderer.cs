12d75c0228370f53ca1f4f703f5ddf337b2dcae9
Alper Keskin <alper.keskin@coda.xyz>
2020-02-02 13:37:05 +0300

    Sprite layer ordering.

---
+++ Assets/Game/Scripts/Utils/SpriteLayerOrderer.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace ggj20
+{
+    public class SpriteLayerOrderer : MonoBehaviour
+    {
+        private SpriteRenderer _renderer;
+
+        private float _bottomY;
+        // Start is called before the first frame update
+        void Start()
+        {
+            var bottomLeft = MapManager.Instance.MainCamera.ViewportToWorldPoint(new Vector3(0, 0));
+            _bottomY = bottomLeft.y;
+            _renderer = GetComponent<SpriteRenderer>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            var bottom = _renderer.bounds.min.y;
+            var d = -Mathf.Abs(bottom - _bottomY) * 100f;
+            _renderer.sortingOrder = (int)d;
+        }
+    }
+}
