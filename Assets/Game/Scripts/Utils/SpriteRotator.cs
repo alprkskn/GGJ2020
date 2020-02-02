be55bbf618b806ae961488f68a6f45bdfc8d8514
Alper Keskin <alper.keskin@coda.xyz>
2020-02-02 12:56:06 +0300

    Mill rotation

---
+++ Assets/Game/Scripts/Utils/SpriteRotator.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace ggj20
+{
+    public class SpriteRotator : MonoBehaviour
+    {
+        [SerializeField] private float _rotationSpeed;
+
+        void Update()
+        {
+            var rot = transform.localRotation;
+
+            rot *= Quaternion.AngleAxis(_rotationSpeed * Time.deltaTime, Vector3.forward);
+
+            transform.localRotation = rot;
+        }
+    }
+}
